8cd9da0039f15feb356c4ba67dab7645142c189a
penglijiang <penglijiang@foxmail.com>
2019-03-03 19:58:23 +0800

    test stack

--- src/stackbase.cpp
+++ src/stackbase.cpp
@@ -7,6 +7,7 @@
 
 
 #include "basestack.hpp"
+#include <stdio.h>
 
 using namespace bs;
 
@@ -14,6 +15,15 @@ int main(){
 
     bsk * pStack = new bsk;
     pStack = push_in_stack(1024,pStack);
+    pStack = push_in_stack(996,pStack);
+    pStack = push_in_stack(985,pStack);
+    pStack = push_in_stack(966,pStack);
+    printlist(pStack);
+    node res;
+    pop_out_stack(res, pStack);
+    printf("val: %d\n", res.val);
+    pop_out_stack(res, pStack);
+    printf("val: %d\n", res.val);
     printlist(pStack);
     return 0;
 }
